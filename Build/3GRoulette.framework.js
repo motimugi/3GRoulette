399319d75a6837eee084026680bde4983251b10f
motimugi <kiei314@icloud.com>
2024-06-27 00:32:41 +0900

    Add replaceAll polyfill for older browsers

--- Build/3GRoulette.framework.js
+++ Build/3GRoulette.framework.js
@@ -1,4 +1,13 @@
+if (!String.prototype.replaceAll) {
+    String.prototype.replaceAll = function(str, newStr){
+        if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+            return this.replace(str, newStr);
+        }
+        return this.replace(new RegExp(str, 'g'), newStr);
+    };
+}
 
+// 既存のコードをここに追加
 var unityFramework = (() => {
   var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
   
